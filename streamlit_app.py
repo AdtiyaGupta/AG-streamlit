e28fd89bc82a0b2eb804708c296bce7bf62ae553
Aditya Gupta <155718055+AdtiyaGupta@users.noreply.github.com>
2024-08-01 21:27:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,34 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+def main():
+    st.set_page_config(
+        page_title="Emulet Login",
+        page_icon="🚀",
+        layout="centered",
+        initial_sidebar_state="collapsed"
+    )
 
-st.write('Hello world!')
+    st.title("Emulet Login")
+
+    col1, col2, col3 = st.columns([0.1, 0.8, 0.1])
+
+    with col2:
+        #st.image("astronaut.png", width=200)
+
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
+
+        sign_in = st.button("Sign In")
+
+        if sign_in:
+            # Add your sign-in logic here
+            st.success("Logged in successfully!")
+
+    col4, col5, col6 = st.columns([0.1, 0.8, 0.1])
+
+    with col5:
+        st.caption("Powered by preprod/corp")
+        #st.image("static_logo.png", width=50)
+
+if __name__ == "__main__":
+    main()

5065a6f9815542461bba36fe503c7373494a1215
Aditya Gupta <155718055+AdtiyaGupta@users.noreply.github.com>
2024-08-02 09:34:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ def main():
 
     with col5:
         st.caption("Powered by preprod/corp")
-        st.image("static_logo.png", width=500)
+        st.image("static_logo.png", width=300)
 
 if __name__ == "__main__":
     main()

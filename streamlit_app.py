5bb2d7934cd9a77cfcf398a5d328253ca062b69d
Aditya Gupta <155718055+AdtiyaGupta@users.noreply.github.com>
2024-08-02 09:31:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,18 +7,7 @@ def main():
         layout="centered",
         initial_sidebar_state="collapsed"
     )
-
-    page_bg_img=
-        <style>
-            body {
-            background-image: url("https://unsplash.com/photos/a-close-up-of-a-cell-phone-with-a-black-background-ThymSQ6OYOw");
-            background-size: cover;
-            }
-        </style>
     
-    
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-
     st.title("Emulet Login")
 
     col1, col2, col3 = st.columns([0.1, 0.8, 0.1])
@@ -39,7 +28,7 @@ def main():
 
     with col5:
         st.caption("Powered by preprod/corp")
-        #st.image("static_logo.png", width=50)
+        st.image("static_logo.png", width=50)
 
 if __name__ == "__main__":
     main()

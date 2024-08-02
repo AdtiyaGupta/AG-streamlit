c35333ab691389d5b7e77660fd207651e2407f41
Aditya Gupta <155718055+AdtiyaGupta@users.noreply.github.com>
2024-08-02 11:07:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,33 +7,7 @@ def main():
         layout="centered",
         initial_sidebar_state="collapsed"
     )
-    
-    
-        
-    st.title("Emulet Login")
-
-    col1, col2, col3 = st.columns([0.1, 0.8, 0.1])
-
-    with col2:
-        #st.image("astronaut.png", width=200)
-
-        username = st.text_input("Username")
-        password = st.text_input("Password", type="password")
-
-        sign_in = st.button("Sign In")
-
-        if sign_in:
-            # Add your sign-in logic here
-            st.success("Logged in successfully!")
-
-    col4, col5, col6 = st.columns([0.1, 0.8, 0.1])
-
-    with col5:
-        st.caption("Powered by preprod/corp")
-        st.image("static_logo.png", width=300)
-
-    
-    background_image_url = st.image("https://wpamelia.com/wp-content/uploads/2019/02/background-black-colors-952670.jpg")
+      background_image_url = st.image("https://wpamelia.com/wp-content/uploads/2019/02/background-black-colors-952670.jpg")
 
      # Create a container with a full-viewport class
     container = st.container()
@@ -60,6 +34,32 @@ def main():
     
     # Add the full-viewport class to the container
     container.add_class("full-viewport")
+    
+        
+    st.title("Emulet Login")
+
+    col1, col2, col3 = st.columns([0.1, 0.8, 0.1])
+
+    with col2:
+        #st.image("astronaut.png", width=200)
+
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
+
+        sign_in = st.button("Sign In")
+
+        if sign_in:
+            # Add your sign-in logic here
+            st.success("Logged in successfully!")
+
+    col4, col5, col6 = st.columns([0.1, 0.8, 0.1])
+
+    with col5:
+        st.caption("Powered by preprod/corp")
+        st.image("static_logo.png", width=300)
+
+    
+  
 
 if __name__ == "__main__":
     main()

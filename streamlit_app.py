ccca5c951757841668f8364d8a8c0dcd65a69531
Aditya Gupta <155718055+AdtiyaGupta@users.noreply.github.com>
2024-08-02 11:17:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,33 +28,7 @@ def main():
             st.success("Logged in successfully!")
 
 
-    background_image_url = st.image("https://wpamelia.com/wp-content/uploads/2019/02/background-black-colors-952670.jpg")
-
-     # Create a container with a full-viewport class
-    container = st.container()
-    
-    # Add CSS style to the container
-    container.markdown(
-        
-        <style>
-        '''
-        .full-viewport {
-            position: fixed;
-            z-index: -1;
-            width: 100%;
-            height: 100%;
-            top: 0;
-            left: 0;
-            background-image: url('""" + background_image_url + """');
-            background-size: cover;
-            background-repeat: no-repeat;
-        }
-        </style>'''
-        ,
-        unsafe_allow_html=False,  # Safe way to inject CSS )
-    
-    # Add the full-viewport class to the container
-    container.add_class("full-viewport")
+  
 
     col4, col5, col6 = st.columns([0.1, 0.8, 0.1])
 

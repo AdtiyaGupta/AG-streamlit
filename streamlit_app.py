32ed90b290d75f6a16660ccf39c11e1d32b8008d
Aditya Gupta <155718055+AdtiyaGupta@users.noreply.github.com>
2024-08-22 21:34:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit_antd_components as sac
 import os
 import pickle
 import pandas as pd
@@ -16,6 +17,26 @@ st.set_page_config(
     page_icon=":rocket:",
     layout="wide",
 )
+
+#Menu Bar
+sac.menu([
+    sac.MenuItem('home', icon='house-fill', tag=[sac.Tag('Tag1', color='green'), sac.Tag('Tag2', 'red')]),
+    sac.MenuItem('products', icon='box-fill', children=[
+        sac.MenuItem('apple', icon='apple'),
+        sac.MenuItem('other', icon='git', description='other items', children=[
+            sac.MenuItem('google', icon='google', description='item description'),
+            sac.MenuItem('gitlab', icon='gitlab'),
+            sac.MenuItem('wechat', icon='wechat'),
+        ]),
+    ]),
+    sac.MenuItem('disabled', disabled=True),
+    sac.MenuItem(type='divider'),
+    sac.MenuItem('link', type='group', children=[
+        sac.MenuItem('antd-menu', icon='heart-fill', href='https://ant.design/components/menu#menu'),
+        sac.MenuItem('bootstrap-icon', icon='bootstrap-fill', href='https://icons.getbootstrap.com/'),
+    ]),
+], size='xl', variant='left-bar', color='grape', open_all=True, return_index=True)
+
 
 # Data Ingestion tab
 st.header("Data Ingestion")

8d995296667261b3bb01c93dba4c51781e56b21d
Aditya Gupta <155718055+AdtiyaGupta@users.noreply.github.com>
2024-08-22 21:49:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,27 +15,28 @@ from sklearn.preprocessing import LabelEncoder
 st.set_page_config(
     page_title="ML model",
     page_icon=":rocket:",
-    layout="centered",
+    layout="wide",
 )
 
 #Menu Bar
-sac.menu([
-    sac.MenuItem('home', icon='house-fill', tag=[sac.Tag('Tag1', color='green'), sac.Tag('Tag2', 'red')]),
-    sac.MenuItem('products', icon='box-fill', children=[
-        sac.MenuItem('apple', icon='apple'),
-        sac.MenuItem('other', icon='git', description='other items', children=[
-            sac.MenuItem('google', icon='google', description='item description'),
-            sac.MenuItem('gitlab', icon='gitlab'),
-            sac.MenuItem('wechat', icon='wechat'),
+with st.sidebar:
+    sac.menu([
+        sac.MenuItem('home', icon='house-fill', tag=[sac.Tag('Tag1', color='green'), sac.Tag('Tag2', 'red')]),
+        sac.MenuItem('products', icon='box-fill', children=[
+            sac.MenuItem('apple', icon='apple'),
+            sac.MenuItem('other', icon='git', description='other items', children=[
+                sac.MenuItem('google', icon='google', description='item description'),
+                sac.MenuItem('gitlab', icon='gitlab'),
+                sac.MenuItem('wechat', icon='wechat'),
+            ]),
         ]),
-    ]),
-    sac.MenuItem('disabled', disabled=True),
-    sac.MenuItem(type='divider'),
-    sac.MenuItem('link', type='group', children=[
-        sac.MenuItem('antd-menu', icon='heart-fill', href='https://ant.design/components/menu#menu'),
-        sac.MenuItem('bootstrap-icon', icon='bootstrap-fill', href='https://icons.getbootstrap.com/'),
-    ]),
-], size='xl', variant='left-bar', color='grape', open_all=True, return_index=True)
+        sac.MenuItem('disabled', disabled=True),
+        sac.MenuItem(type='divider'),
+        sac.MenuItem('link', type='group', children=[
+            sac.MenuItem('antd-menu', icon='heart-fill', href='https://ant.design/components/menu#menu'),
+            sac.MenuItem('bootstrap-icon', icon='bootstrap-fill', href='https://icons.getbootstrap.com/'),
+        ]),
+    ], size='xl', variant='left-bar', color='grape', open_all=True, return_index=True)
 
 
 # Data Ingestion tab
